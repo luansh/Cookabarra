653d39faa030e394b66ccb33836a6af8a26882a2
luansh0310@163.com <luansh0310@163.com>
2024-03-22 05:35:52 +0800

    :pencil:refine

--- rtl/obj_dir/Vsimple_system__Dpi.d
+++
@@ -1,8 +0,0 @@
-Vsimple_system__Dpi.o: Vsimple_system__Dpi.cpp Vsimple_system__Dpi.h \
- /usr/local/verilator/share/verilator/include/vltstd/svdpi.h \
- Vsimple_system.h \
- /usr/local/verilator/share/verilator/include/verilated.h \
- /usr/local/verilator/share/verilator/include/verilatedos.h \
- /usr/local/verilator/share/verilator/include/verilated_config.h \
- /usr/local/verilator/share/verilator/include/verilated_types.h \
- /usr/local/verilator/share/verilator/include/verilated_funcs.h
--- rtl/obj_dir/Vsimple_system__Syms.d
+++
@@ -1,9 +0,0 @@
-Vsimple_system__Syms.o: Vsimple_system__Syms.cpp Vsimple_system__Syms.h \
- /usr/local/verilator/share/verilator/include/verilated.h \
- /usr/local/verilator/share/verilator/include/verilatedos.h \
- /usr/local/verilator/share/verilator/include/verilated_config.h \
- /usr/local/verilator/share/verilator/include/verilated_types.h \
- /usr/local/verilator/share/verilator/include/verilated_funcs.h \
- Vsimple_system.h \
- /usr/local/verilator/share/verilator/include/vltstd/svdpi.h \
- Vsimple_system___024root.h
